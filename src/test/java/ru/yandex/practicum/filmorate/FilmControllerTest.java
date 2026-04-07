fea171d0cda9c6ed14829f4f502f0d666d7a5b80
Evgeniy <gladskih07@yandex.ru>
2026-04-07 22:22:05 +0300

    исправление замечаний checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -1,21 +1,37 @@
 package ru.yandex.practicum.filmorate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FilmControllerTest {
-    private final FilmController controller = new FilmController();
+
+    private FilmController controller;
+
+    @BeforeEach
+    void setUp() {
+        FilmStorage filmStorage = new InMemoryFilmStorage();
+        UserStorage userStorage = new InMemoryUserStorage();
+        FilmService filmService = new FilmService(filmStorage, userStorage);
+
+        controller = new FilmController(filmStorage, filmService);
+    }
 
     @Test
     void shouldNotValidateFilmWithEmptyName() {
         Film film = new Film();
-        film.setName(""); // Пустое имя
+        film.setName("");
         film.setDescription("Описание");
         film.setReleaseDate(LocalDate.now());
         film.setDuration(100);
@@ -27,7 +43,7 @@ class FilmControllerTest {
     void shouldNotValidateFilmWithLongDescription() {
         Film film = new Film();
         film.setName("Имя");
-        film.setDescription("a".repeat(201)); // Описание > 200 символов
+        film.setDescription("a".repeat(201));
         film.setReleaseDate(LocalDate.now());
         film.setDuration(100);
 
@@ -38,7 +54,8 @@ class FilmControllerTest {
     void shouldNotValidateFilmWithOldReleaseDate() {
         Film film = new Film();
         film.setName("Имя");
-        film.setReleaseDate(LocalDate.of(1800, 1, 1)); // Слишком старая дата
+        film.setReleaseDate(LocalDate.of(1800, 1, 1));
+        film.setDuration(100);
 
         assertThrows(ValidationException.class, () -> controller.create(film));
     }
